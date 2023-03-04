4259ed04d09bed566de2b64eebe02a4ada2678d9
waynebeam <waynejcbeam@me.com>
2023-03-04 08:54:21 -0500

    Going to index directs to blank login page

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, session, render_template, redirect, url_for
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
+app.secret_key = os.environ['FLASK_SECRET_KEY']
+
 @app.route("/")
 def test_homepage():
-    return "<h1>hello world!</p>"
+    if 'username' in session:
+        return "<h1>hello User!</p>"
+    return redirect(url_for('show_login_form'))
+
+@app.get("/login")
+def show_login_form():
+    return render_template("login.html")
