91a68da0421daa320026adac1a8e645dcd74b54f
waynebeam <waynejcbeam@me.com>
2023-03-04 09:53:39 -0500

    database.py created and linked to railway postgres DB

---
+++ database.py
@@ -0,0 +1,10 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# with psycopg2.connect(os.environ['DB_CONNECTION_STRING']) as conn:
+#     with conn.cursor() as cur:
+        
+        
