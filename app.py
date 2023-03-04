38f3217a8536e450c8e093bd56b46e28a5e1186e
waynebeam <waynejcbeam@me.com>
2023-03-04 09:26:28 -0500

    basic route loop for login or make new account

--- app.py
+++ app.py
@@ -21,3 +21,11 @@ def show_login_form():
 @app.post("/login")
 def do_the_login():
     return "<p>Got the login info! Processing.</p>"
+
+@app.get("/create-account")
+def show_account_creation():
+    return render_template("create-account.html")
+
+@app.post("/create-account")
+def create_new_account():
+    return "<p>New account created!</p>"
